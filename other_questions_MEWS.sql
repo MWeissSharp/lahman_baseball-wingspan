8642e10443f243cf36708284b35389cb3f920cfc
MWeissSharp <mews27@gmail.com>
2022-06-09 11:06:47 -0500

    Final work on this project, have initial work on all bonus questions and a different way of finding the last part of question 7

--- other_questions_MEWS.sql
+++ other_questions_MEWS.sql
@@ -236,6 +236,23 @@ FROM (SELECT *,
 WHERE win_rank_by_year = 1
 AND wswin = 'Y';
 --25.53% of the time, the team with the most wins also won the world series between 1970 and 2016
+--Trying to answer the last part without a window function
+WITH topwswinners AS ((SELECT yearid,
+							 MAX(w) AS w
+						FROM teams
+						WHERE yearid BETWEEN 1970 AND 2016
+						GROUP BY yearid
+						ORDER BY yearid)
+						INTERSECT
+						(SELECT yearid,
+							    w
+						FROM teams
+						WHERE wswin = 'Y'
+						AND yearid BETWEEN 1970 AND 2016
+						ORDER BY yearid))
+SELECT teams.name, teams.yearid, teams.w, teams.wswin
+FROM teams INNER JOIN topwswinners ON teams.yearid = topwswinners.yearid AND teams.w = topwswinners.w
+WHERE teams.wswin = 'Y';
 
 --Question #8
 /*Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance 
@@ -323,3 +340,238 @@ also got 2 hr in 2009))*/
 /*Is there any correlation between number of wins and team salary? Use data from 2000 and later to answer this question. 
 As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want 
 to look on a year-by-year basis.*/
+WITH t_salaries AS	(SELECT teamid,
+					 		yearid,
+					 		SUM(salary)::numeric::money AS team_total_salary
+					 FROM salaries
+					 WHERE yearid >= 2000
+					 GROUP BY teamid, yearid
+					 ORDER BY teamid, yearid)
+SELECT ts.teamid,
+	   ts.yearid,
+	   ts.team_total_salary,
+	   RANK() OVER(PARTITION BY ts.yearid ORDER BY ts.team_total_salary DESC) AS salary_rank,
+	   t.w AS wins,
+	   RANK() OVER(PARTITION BY ts.yearid ORDER BY t.w DESC) AS win_count_rank,
+	   t.g AS total_games,
+	   ROUND(t.w::decimal / t.g::decimal * 100, 2) AS percent_wins,
+	   RANK() OVER(PARTITION BY ts.yearid ORDER BY ROUND(t.w::decimal / t.g::decimal * 100, 2) DESC) AS win_percent_rank
+FROM t_salaries AS ts INNER JOIN teams AS t ON ts.teamid = t.teamid AND ts.yearid = t.yearid
+
+--Question #12
+/* In this question, you will explore the connection between number of wins and attendance.
+    a) Does there appear to be any correlation between attendance at home games and number of wins?
+    b) Do teams that win the world series see a boost in attendance the following year? 
+	   What about teams that made the playoffs? Making the playoffs means either being a division winner 
+	   or a wild card winner.*/
+--Part A
+--Table below gives attendance and homegames for each team each year
+SELECT DISTINCT team,
+	   		 	year,
+			    SUM(attendance) OVER(PARTITION BY team, year) AS homegame_total_attendance,
+	  			SUM(games) OVER(PARTITION BY team, year) AS homegames
+FROM homegames
+ORDER BY year, team;
+--Pulling in team name and wins
+WITH hg AS	(SELECT DISTINCT team,
+	   		 				 year,
+			 				 SUM(attendance) OVER(PARTITION BY team, year) AS homegame_total_attendance,
+	   		 				 SUM(games) OVER(PARTITION BY team, year) AS homegames
+			 FROM homegames
+			 ORDER BY year, team)
+SELECT t.name,
+	   hg.year,
+	   hg.homegame_total_attendance,
+	   hg.homegames,
+	   ROUND(hg.homegame_total_attendance/hg.homegames, 0) AS homegame_attendance_per_game,
+	   t.w AS wins,
+	   t.g AS total_games
+FROM hg LEFT JOIN teams AS t ON hg.team = t.teamid AND hg.year = t.yearid
+ORDER BY hg.year, t.name;
+
+--Part B
+--I'm only including my final solution here, the script for just question 12 has more of my work
+--Using LEAD function for world series winners
+SELECT *
+FROM 	(WITH hg AS	(SELECT DISTINCT team,
+									 year,
+									 SUM(attendance) OVER(PARTITION BY team, year) AS homegame_total_attendance,
+									 SUM(games) OVER(PARTITION BY team, year) AS homegames
+					 FROM homegames)
+		SELECT t.name,
+			   hg.year AS wswin_year,
+			   hg.homegame_total_attendance,
+			   hg.homegames,
+		 	   t.wswin,
+			   ROUND(hg.homegame_total_attendance/hg.homegames, 0) AS homegame_attendance_per_game,
+			   LEAD(hg.homegame_total_attendance, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_hg_total_attendance,
+			   LEAD(hg.homegames, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_homegames,
+			   LEAD(ROUND(hg.homegame_total_attendance/hg.homegames, 0), 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_hg_attendance_per_game
+		FROM hg INNER JOIN teams AS t ON hg.team = t.teamid AND hg.year = t.yearid) AS year_comparisons
+WHERE wswin = 'Y'
+ORDER BY wswin_year;
+--Doing calculations on the attendance change
+SELECT name,
+	   year AS wswin_year,
+	   (ny_hg_total_attendance - homegame_total_attendance) AS total_attendance_change,
+	   ny_hg_attendance_per_game - homegame_attendance_per_game AS per_game_attendance_change,
+	   ny_homegames - homegames AS hg_count_change
+FROM 	(WITH hg AS	(SELECT DISTINCT team,
+									 year,
+									 SUM(attendance) OVER(PARTITION BY team, year) AS homegame_total_attendance,
+									 SUM(games) OVER(PARTITION BY team, year) AS homegames
+					 FROM homegames)
+		SELECT t.name,
+			   hg.year,
+			   hg.homegame_total_attendance,
+			   hg.homegames,
+			   ROUND(hg.homegame_total_attendance/hg.homegames, 0) AS homegame_attendance_per_game,
+			   t.wswin,
+			   LEAD(hg.year, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS next_year,
+			   LEAD(hg.homegame_total_attendance, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_hg_total_attendance,
+			   LEAD(hg.homegames, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_homegames,
+			   LEAD(ROUND(hg.homegame_total_attendance/hg.homegames, 0), 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_hg_attendance_per_game
+		FROM hg INNER JOIN teams AS t ON hg.team = t.teamid AND hg.year = t.yearid) AS year_comparisons
+WHERE wswin = 'Y'
+ORDER BY wswin_year;
+--AVG changes
+SELECT ROUND(AVG(ny_hg_total_attendance - homegame_total_attendance), 0) AS avg_total_attendance_change,
+	   ROUND(AVG(ny_hg_attendance_per_game - homegame_attendance_per_game), 0) AS avg_per_game_attendance_change
+FROM 	(WITH hg AS	(SELECT DISTINCT team,
+									 year,
+									 SUM(attendance) OVER(PARTITION BY team, year) AS homegame_total_attendance,
+									 SUM(games) OVER(PARTITION BY team, year) AS homegames
+					 FROM homegames)
+		SELECT t.name,
+			   hg.year,
+			   hg.homegame_total_attendance,
+			   hg.homegames,
+			   ROUND(hg.homegame_total_attendance/hg.homegames, 0) AS homegame_attendance_per_game,
+			   t.wswin,
+			   LEAD(hg.year, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS next_year,
+			   LEAD(hg.homegame_total_attendance, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_hg_total_attendance,
+			   LEAD(hg.homegames, 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_homegames,
+			   LEAD(ROUND(hg.homegame_total_attendance/hg.homegames, 0), 1) OVER(PARTITION BY t.name ORDER BY t.yearid) AS ny_hg_attendance_per_game
+		FROM hg INNER JOIN teams AS t ON hg.team = t.teamid AND hg.year = t.yearid) AS year_comparisons
+WHERE wswin = 'Y';
+--I'm not reproducing for the division/wild card winners here, but you would do the same thing, just adjust your WHERE statement accordingly
+
+--Question #13
+/*It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. 
+Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed 
+pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? 
+Are they more likely to make it into the hall of fame?*/
+--Getting a list of all pitchers from the people table
+SELECT playerid, namefirst, namelast, throws
+FROM people
+WHERE playerid IN
+				(SELECT playerid
+				 FROM pitching);
+
+SELECT COUNT(DISTINCT playerid)
+FROM people
+WHERE playerid IN
+				(SELECT playerid
+				 FROM pitching);
+--9302 pitchers
+--Getting all entry options for the throws column
+SELECT DISTINCT throws
+FROM people
+WHERE playerid IN
+				(SELECT playerid
+				 FROM pitching);
+--R, L, null, and S are the options
+--Getting counts for each option
+SELECT 
+	COUNT(CASE WHEN throws = 'R' THEN playerid END) AS r_handed_pitchers,
+	COUNT(CASE WHEN throws = 'L' THEN playerid END) AS l_handed_pitchers,
+	COUNT(CASE WHEN throws IS NULL THEN playerid END) AS unknown,
+	COUNT(CASE WHEN throws = 'S' THEN playerid END) AS s_pitchers
+FROM people
+WHERE playerid IN
+				(SELECT playerid
+				 FROM pitching);
+-- R=6605, L=2477, NULL=219, S=1
+--Cy Young Award winners
+SELECT playerid, namefirst, namelast, throws
+FROM people
+WHERE playerid IN
+				(SELECT DISTINCT playerid
+				FROM awardsplayers
+				WHERE awardid = 'Cy Young Award');
+--Counts for handedness for Cy Young Award winners (there are no null or S pitchers in this group)
+SELECT 
+	COUNT(CASE WHEN throws = 'R' THEN playerid END) AS r_handed_pitchers,
+	COUNT(CASE WHEN throws = 'L' THEN playerid END) AS l_handed_pitchers
+FROM people
+WHERE playerid IN
+				(SELECT playerid
+				FROM awardsplayers
+				WHERE awardid = 'Cy Young Award');
+--53 right handed, 24 left handed
+--Combining all pitcher info w/ Cy Young award info, award was first given in 1956, so limiting to those years
+WITH all_pitchers AS (SELECT playerid,
+					  namefirst,
+					  namelast,
+					  throws
+					  FROM people
+					  WHERE playerid IN
+									(SELECT playerid
+									 FROM pitching
+									 WHERE yearid >=1956)),
+	 cy_winners AS	(SELECT playerid, awardid
+					 FROM awardsplayers
+					 WHERE awardid = 'Cy Young Award')
+SELECT namefirst, namelast, throws, awardid
+FROM all_pitchers FULL JOIN cy_winners USING(playerid);
+--Getting percentages
+SELECT ROUND(r_handed_winners::decimal / r_handed_pitchers::decimal * 100, 2) AS percent_of_r_handed,
+	   ROUND(l_handed_winners::decimal / l_handed_pitchers::decimal * 100, 2) AS percent_of_l_handed
+FROM(WITH all_pitchers AS (SELECT playerid,
+							  namefirst,
+							  namelast,
+							  throws
+							  FROM people
+							  WHERE playerid IN
+											(SELECT playerid
+											 FROM pitching
+											 WHERE yearid >=1956)),
+			 cy_winners AS	(SELECT DISTINCT(playerid), awardid
+							 FROM awardsplayers
+							 WHERE awardid = 'Cy Young Award')
+		SELECT
+			COUNT(CASE WHEN throws = 'R' THEN playerid END) AS r_handed_pitchers,
+			COUNT(CASE WHEN throws = 'L' THEN playerid END) AS l_handed_pitchers,
+			COUNT(CASE WHEN throws IS NULL THEN playerid END) AS unknown,
+			COUNT(CASE WHEN throws = 'S' THEN playerid END) AS s_pitchers,
+			COUNT(CASE WHEN throws = 'R' AND awardid = 'Cy Young Award' THEN playerid END) AS r_handed_winners,
+			COUNT(CASE WHEN throws = 'L' AND awardid = 'Cy Young Award' THEN playerid END) AS l_handed_winners
+		FROM all_pitchers LEFT JOIN cy_winners USING(playerid)) AS counts;
+--1.32% of right handed pitchers since 1956 became Cy Young Award winners, 1.51% of left handed pitchers
+--Hall of fame calculations
+SELECT ROUND(r_handed_winners::decimal / r_handed_pitchers::decimal * 100, 2) AS percent_of_r_handed,
+	   ROUND(l_handed_winners::decimal / l_handed_pitchers::decimal * 100, 2) AS percent_of_l_handed
+FROM(WITH all_pitchers AS (SELECT playerid,
+							  namefirst,
+							  namelast,
+							  throws
+							  FROM people
+							  WHERE playerid IN
+											(SELECT playerid
+											 FROM pitching)),
+			 hof_winners AS	(SELECT playerid, yearid,
+							 inducted AS hof_inductee
+							 FROM halloffame
+							 WHERE inducted = 'Y'
+							 AND category = 'Player')
+		SELECT
+			COUNT(CASE WHEN throws = 'R' THEN playerid END) AS r_handed_pitchers,
+			COUNT(CASE WHEN throws = 'L' THEN playerid END) AS l_handed_pitchers,
+			COUNT(CASE WHEN throws IS NULL THEN playerid END) AS unknown,
+			COUNT(CASE WHEN throws = 'S' THEN playerid END) AS s_pitchers,
+			COUNT(CASE WHEN throws = 'R' AND hof_inductee = 'Y' THEN playerid END) AS r_handed_winners,
+			COUNT(CASE WHEN throws = 'L' AND hof_inductee = 'Y' THEN playerid END) AS l_handed_winners
+		FROM all_pitchers LEFT JOIN hof_winners USING(playerid)) AS counts;
+--1.09% of right handed pitchers have gone on to be inducted into the hall of fame, 0.89% of left handed pitchers 
+
+
